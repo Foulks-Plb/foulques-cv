ed9596a5eb51a81505f8631aaab343d8ca5d3e46
Foulques pellabeuf <foulques.pellabeuf@onbrane.com>
2024-05-21 22:19:26 +0200

    feat: reponsive tools component

--- components/tools/tools.tsx
+++ components/tools/tools.tsx
@@ -1,6 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Tools() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const download = async () => {
     const response = await fetch("/foulques_pellabeuf_cv.pdf");
     const blob = await response.blob();
@@ -22,7 +38,7 @@ export default function Tools() {
   return (
     <>
       <div className="absolute right-0 m-2 inline-flex items-center">
-        <label className="inline-flex items-center cursor-pointer ml-4">
+        <label className="inline-flex items-center cursor-pointer mr-4">
           <input
             type="checkbox"
             value=""
@@ -31,19 +47,21 @@ export default function Tools() {
           />
           <div className="relative w-11 h-6 bg-white-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         </label>
-        <button
-          onClick={() => download()}
-          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-        >
-          <svg
-            className="fill-current w-4 h-4 mr-2"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
+        {!isMobile && (
+          <button
+            onClick={() => download()}
+            className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
           >
-            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-          </svg>
-          <span>Download</span>
-        </button>
+            <svg
+              className="fill-current w-4 h-4 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+            </svg>
+            <span>Download</span>
+          </button>
+        )}
       </div>
     </>
   );
