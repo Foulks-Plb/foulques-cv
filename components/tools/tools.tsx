167d40bd594ff80d3567fe89d6d7b0b26e8041a0
Foulques pellabeuf <foulques.pellabeuf@onbrane.com>
2024-05-21 22:11:19 +0200

    fix: tools to left

--- components/tools/tools.tsx
+++ components/tools/tools.tsx
@@ -21,10 +21,19 @@ export default function Tools() {
 
   return (
     <>
-      <div className="absolute left-0 mt-2 ml-2 inline-flex items-center">
+      <div className="absolute right-0 m-2 inline-flex items-center">
+        <label className="inline-flex items-center cursor-pointer ml-4">
+          <input
+            type="checkbox"
+            value=""
+            className="sr-only peer"
+            onChange={toggleTheme}
+          />
+          <div className="relative w-11 h-6 bg-white-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        </label>
         <button
           onClick={() => download()}
-          className="mr-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+          className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
         >
           <svg
             className="fill-current w-4 h-4 mr-2"
@@ -35,15 +44,6 @@ export default function Tools() {
           </svg>
           <span>Download</span>
         </button>
-        <label className="inline-flex items-center cursor-pointer">
-          <input
-            type="checkbox"
-            value=""
-            className="sr-only peer"
-            onChange={toggleTheme}
-          />
-          <div className="relative w-11 h-6 bg-white-200 peer-focus:outline-none peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        </label>
       </div>
     </>
   );
