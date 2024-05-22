f702e7b5935df229f9905ee3eca7cf84151b35e4
Foulques pellabeuf <foulques.pellabeuf@onbrane.com>
2024-05-22 12:10:25 +0200

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+import Head from "next/head";
 
-const inter = Poppins({ subsets: ["latin"], weight: ["400", "700" , "800", "900", "100", "200", "300", "500", "600"]});
+const inter = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700", "800", "900", "100", "200", "300", "500", "600"],
+});
 
 export const metadata: Metadata = {
   title: "Foulques Pellabeuf",
@@ -16,7 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <Head>
+        <title>Foulques Pellabeuf</title>
+        <meta property="og:title" content="Foulques Pellabeuf" key="title" />
+      </Head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
